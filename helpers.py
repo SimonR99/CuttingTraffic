e6b5c044d57af2d8b37cc9baff6f0c6028a5f6ef
Simon Roy <simonroy99@hotmail.ca>
2024-02-22 21:32:52 +0000

    created pdf notebook

---
+++ helpers.py
@@ -0,0 +1,9 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+def draw_graph(G, colors, pos):
+    default_axes = plt.axes(frameon=True)
+    nx.draw_networkx(G, node_color=colors, node_size=600, alpha=0.8, ax=default_axes, pos=pos)
+    edge_labels = nx.get_edge_attributes(G, "weight")
+    nx.draw_networkx_edge_labels(G, pos=pos, edge_labels=edge_labels)
